9692ed386222fda265db6b7b4f91e59fbf113438
aqlitz2 <aqlitz.biznes@gmail.com>
2025-03-11 11:17:07 +0100

    to zroblem i tamto

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,43 @@
 ﻿
+
+
+
+using System.ComponentModel.Design;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a -= b;
+                continue;
+            }
+            else
+            {
+                b -= a;
+                continue;
+            }
+
+        } Console.Write("Najwiekszy wspolny dzielnik to " + a); 
+       
+    
+    
+    
+    }
+
+
+}
+    
+       
+
+
+
